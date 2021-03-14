1c8b116e76ef719952206bbc77835d8d16c53f27
Uladzislau Dolnikau <v-udolnikau@expedia.com>
2021-03-14 22:40:37 +0300

    Initial db integration

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mysql = require('mysql');
 var router = express.Router();
 
 var awskey = 'AKIA3SWAVHGBVCTRO7GX';
@@ -7,114 +8,46 @@ var awsregion = 'us-east-1';
 var AWS = require('aws-sdk');
 AWS.config.update({accessKeyId: awskey, secretAccessKey: awssecretkey, region: awsregion});
 var s3 = new AWS.S3();
+var dbConnection = mysql.createConnection({
+    host: 'image-viewer.c4eskojl0xwl.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'adminadmin',
+    database: 'images'
+})
+dbConnection.connect();
 
-//var IMAGE_TYPEFILTER = process.env.IMAGE_TYPEFILTER || '.png,.jpg';
-
-//----------------------------------------------------------------------------
-// validate the images and filter them according to prefs
-//----------------------------------------------------------------------------
-function filterImages(data) {
-    const encodedImages = [];
-    // data.forEach(img=> {
-        let buf = Buffer.from(data);
-        let base64 = buf.toString('base64');
-        encodedImages.push(base64);
-    // });
-    return encodedImages;
-}
-
-//----------------------------------------------------------------------------
-// loop through S3 formatted API results and build an images list
-//----------------------------------------------------------------------------
-async function buildImagesListFromS3Data(bucketname, data) {
-    const S3_PREFIX = 'https://s3.amazonaws.com/' + bucketname + '/';
-     var images = [];
-    // var contents = data.Contents
-    // console.log("iterating " + JSON.stringify(contents));
-    // for (var iter in contents) {
-    //     any validation of key can go here
-    //     console.log("adding " + S3_PREFIX + contents[iter].Key)
-        // images.push(S3_PREFIX + contents[iter].Key);
-    // }
-    let items = await (s3.getObject(
-        {
-            Bucket: bucketname,
-            Key: '96802-1.jpg'
-        }
-    ).promise()).Body;
-    console.log(items);
-    images.push(items);
-    
-    return images;
-}
-
-//----------------------------------------------------------------------------
-// GET on the main page.
-//----------------------------------------------------------------------------
 router.get('/', function (req, res, next) {
-
-    // TODO pull these from ENV vars + user added buckets
-    var bucketIds = ['images-store-bucket'];
-
-    var imagesArray = [];
-    var showBucket = req.query.showBucket;
-    if (!showBucket) {
-        console.log('no bucket, forcing to first bucket');
-        showBucket = bucketIds[0]
-    }
-    console.log('loading bucket: ' + showBucket);
-
-    // for DEBUGGING
-    if (showBucket === 'FakeBucket') {
-        imagesArray = ['https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fweekend%2F&psig=AOvVaw3Rud8NdupqGA75h8PSjHVX&ust=1615804542835000&source=images&cd=vfe&ved=0CAYQjRxqFwoTCNCDoJHLr-8CFQAAAAAdAAAAABAD'];
-        res.render('index', {
-            title: 'AWS S3 Image Viewer',
-            showBucket: showBucket,
-            images: JSON.stringify(imagesArray),
-            buckets: JSON.stringify(bucketIds)
-        });
-    } else {
-        // query for images
-        console.log('querying S3 for objects in ' + showBucket);
-        var params = {
-            Bucket: showBucket
-            //ContinuationToken: 'STRING_VALUE',
-            //Delimiter: 'STRING_VALUE',
-            //EncodingType: url,
-            //FetchOwner: false,
-            //MaxKeys: 50,
-            //Prefix: 'STRING_VALUE',
-            //RequestPayer: requester,
-            //StartAfter: 'STRING_VALUE'
-        };
-        s3.listObjectsV2(params, function (err, data) {
-            if (err) {
-                console.log(err, err.stack); // an error occurred
-            } else {
-                const keys = data.Contents.map(content => content.Key);
-                let promises = [], images = [];
-                keys.forEach(key => {
-                    promises.push(s3.getObject({"Bucket": 'images-store-bucket', "Key": key}).promise());
-                });
-                Promise.all(promises).then(s3Responses => {
-                    s3Responses.forEach(s3Response => {
-                        let image = new Buffer(s3Response.Body).toString('base64');
-                        image = "data:" + s3Response.ContentType + ";base64," + image;
-                        images.push(image);
-                    })
-                    res.render('index', {
-                        title: 'AWS S3 Image Viewer',
-                        showBucket: showBucket,
-                        images: images,
-                        buckets: JSON.stringify(bucketIds)
-                    });
+    dbConnection.query('SELECT * from metadata AS metadata', null, function (err, rows) {
+        if (err) {
+            console.log(err, err.stack);
+        } else {
+            const titles = rows.map(row => row.title);
+            let promises = [], images = [];
+            rows.forEach(row => {
+                let imagePromise = s3.getObject({"Bucket": 'images-store-bucket', "Key": row.s3Key}).promise();
+                promises.push(imagePromise);
+            });
+            Promise.all(promises).then(promise => {
+                promise.forEach((s3ResponseImage, index) => {
+                    let image = new Buffer(s3ResponseImage.Body).toString('base64');
+                    image = {
+                        image: "data:" + s3ResponseImage.ContentType + ";base64," + image,
+                        title: titles[index]
+                    };
+                    images.push(image);
                 })
-            }
-        });
-    }
+                res.render('index', {
+                    title: 'AWS S3 Image Viewer',
+                    showBucket: req.query.showBucket,
+                    images: images,
+                    buckets: JSON.stringify(['images-store-bucket'])
+                });
+            })
+        }
+    });
 });
 
-router.post('/index', function(req, res) {
+router.post('/index', function (req, res) {
     console.log('yoohuoo');
 });
 
@@ -124,20 +57,20 @@ async function getImageByName() {
         "Key": "96802-1.jpg"
     };
     const data = await s3.getObject(params).promise();
-    
+
     let image = new Buffer(data.Body).toString('base64');
     image = "data:" + data.ContentType + ";base64," + image;
     return [image];
 }
 
-router.get('/imageId', function(req, res, next) {
+router.get('/imageId', function (req, res, next) {
     var params = {
         "Bucket": 'images-store-bucket',
         "Key": "96802-1.jpg"
     };
     s3.getObject(params, function (err, data) {
         if (err) {
-            console.log(err, err.stack); // an error occurred
+            console.log(err, err.stack);
         } else {
             let image = new Buffer(data.Body).toString('base64');
             image = "data:" + data.ContentType + ";base64," + image;
@@ -152,6 +85,8 @@ router.get('/imageId', function(req, res, next) {
             };
             res.send(response);
         }
-    });});
+    });
+});
 
 module.exports = router;
+
