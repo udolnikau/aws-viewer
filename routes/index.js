0bea5b5c13483917cc58d0a28bb2e723c7d357e0
Uladzislau Dolnikau <v-udolnikau@expedia.com>
2021-03-15 09:24:22 +0300

    Upload integration

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var mysql = require('mysql');
-var router = express.Router();
+var AWS = require('aws-sdk');
 
+var router = express.Router();
 var awskey = 'AKIA3SWAVHGBVCTRO7GX';
 var awssecretkey = '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';
 var awsregion = 'us-east-1';
-var AWS = require('aws-sdk');
 AWS.config.update({accessKeyId: awskey, secretAccessKey: awssecretkey, region: awsregion});
 var s3 = new AWS.S3();
+
+var multer = require('multer');
+var upload = multer({ storage: multer.memoryStorage() });
+
 var dbConnection = mysql.createConnection({
     host: 'image-viewer.c4eskojl0xwl.us-east-1.rds.amazonaws.com',
     user: 'admin',
@@ -16,7 +20,7 @@ var dbConnection = mysql.createConnection({
 })
 dbConnection.connect();
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     dbConnection.query('SELECT * from metadata AS metadata', null, function (err, rows) {
         if (err) {
             console.log(err, err.stack);
@@ -47,26 +51,71 @@ router.get('/', function (req, res, next) {
     });
 });
 
-router.post('/index', function (req, res) {
-    console.log('yoohuoo');
+router.post('/upload', upload.single('bucketNameTextInput'), function (req, res, next) {
+    const params = {
+        Key: req.file.originalname,
+        Body: req.file.buffer,
+        ContentEncoding: 'base64',
+        ContentType: 'image/jpeg',
+        ACL: 'public-read',
+        Bucket: 'images-store-bucket',
+    }
+    return new Promise((resolve, reject) => {
+        s3.putObject(params, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                dbConnection.query('INSERT INTO metadata (s3Key, title) values (?,?)', 
+                    [req.file.originalname, req.file.originalname], function (err) {
+                    if (err) {
+                        console.log(err, err.stack);
+                    } else {
+                        resolve('success');
+                        res.send({"statusCode": 200})
+                    }
+                });
+            }
+        })
+    })
 });
 
-async function getImageByName() {
+router.get('/random', function (req, res, next) {
+    dbConnection.query('SELECT * from metadata AS metadata', null, function (err, rows) {
+        if (err) {
+            console.log(err, err.stack);
+        } else {
+            let keys = rows.map(row => row.s3Key);
+            const key = keys[Math.floor(Math.random() * keys.length)];
+            var params = {
+                "Bucket": 'images-store-bucket',
+                "Key": key
+            };
+            s3.getObject(params, function (err, data) {
+                if (err) {
+                    console.log(err, err.stack);
+                } else {
+                    let image = new Buffer(data.Body).toString('base64');
+                    image = "data:" + data.ContentType + ";base64," + image;
+                    let response = {
+                        "statusCode": 200,
+                        "headers": {
+                            "Access-Control-Allow-Origin": "*",
+                            'Content-Type': data.ContentType
+                        },
+                        "body": image,
+                        "isBase64Encoded": true
+                    };
+                    res.send(response);
+                }
+            });
+        }
+    });
+});
+
+router.get('/:imageId', function (req, res, next) {
     var params = {
         "Bucket": 'images-store-bucket',
-        "Key": "96802-1.jpg"
-    };
-    const data = await s3.getObject(params).promise();
-
-    let image = new Buffer(data.Body).toString('base64');
-    image = "data:" + data.ContentType + ";base64," + image;
-    return [image];
-}
-
-router.get('/imageId', function (req, res, next) {
-    var params = {
-        "Bucket": 'images-store-bucket',
-        "Key": "96802-1.jpg"
+        "Key": req.params.imageId
     };
     s3.getObject(params, function (err, data) {
         if (err) {
